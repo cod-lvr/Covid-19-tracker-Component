63c6cd24e6b52b7a618ab9f324f800c8cd2cf82a
cod-lvr <aymang29@yahoo.com>
2021-06-24 05:33:52 +0200

    add datadisplay compnent

--- src/components/Display/DataDisplay.js
+++ src/components/Display/DataDisplay.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Fragment } from 'react-is';
-import styles from './DataDisplay.moddule.css';
+import React from "react";
+import { Fragment } from "react-is";
+import styles from "./DataDisplay.module.css";
+import Card from './Card';
 
-const DataDisplay = props => {
-    return (
-        <Fragment>
-            <seaction className={styles.boxes}>
-                
-            </seaction>
-        </Fragment>
-    )
-}
+const DataDisplay = (props) => {
+  return (
+    <Fragment>
+      <section className={styles.boxes}>
+        {props.countries.map((country) => {
+         return <Card
+            id={country.code}
+            country={country.country}
+            confirmed={country.todayCases}
+            deaths={country.deaths}
+            cases={country.cases}
+          />;
+        })}
+        
+      </section>
+    </Fragment>
+  );
+};
 
 export default DataDisplay;
