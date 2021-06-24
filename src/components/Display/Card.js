c1f0a5d042a59d058e5fa24cd79c10735da95328
cod-lvr <aymang29@yahoo.com>
2021-06-24 05:32:42 +0200

    add card component

--- src/components/Display/Card.js
+++ src/components/Display/Card.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import styles from './Card.module.css';
+import React from "react";
+import styles from "./Card.module.css";
 
-const Card = props  => {
-    return (
-        <div className={styles.Card}>
-            
-        </div>
-    );
-}
+const Card = (props) => {
+  return (
+    <ul className={styles.Card}>
+      <li key={props.id}>
+        <h2>{props.country}</h2>
+        <h3>{props.confirmed}</h3>
+        <h3>{props.deaths}</h3>
+        <h3>{props.cases}</h3>
+      </li>
+    </ul>
+  );
+};
 
 export default Card;
